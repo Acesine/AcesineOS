a15c40a6c4c5a05ed5d37b8d2da043dfff773c53
acesine <xianggaodave@gmail.com>
2016-12-26 00:02:16 -0800

    colorful hello world in protected mode

--- src/kernel.c
+++ src/kernel.c
@@ -1,9 +1,23 @@
+void sleep() {
+  int i=0;
+  while (i < 25000000) i ++;
+}
+
+void change_background(char* c) {
+  if (++(*c) > 15) *c = 0;
+}
+
 void main() {
-  char background = 0x0C;
-  char message[] = "Welcome!";
+  char message[] = "Hello World!";
   char* video_memory = 0xB8000;
-  for (int i=0; i<sizeof(message); i++) {
-    *(video_memory++) = message[i];
-    *(video_memory++) = background;
+  char background = 0;
+  while (1) {
+    for (int i=0; i<sizeof(message); i++) {
+      *(video_memory++) = message[i];
+      *(video_memory++) = background;
+      change_background(&background);
+    }
+    video_memory = 0xB8000;
+    sleep();
   }
 }
